844bd1eb506acdf28c30a08f0762692527c94510
Diogo Peres <plaer654@gmail.com>
2024-05-28 16:45:15 -0300

    finalização

---
+++ src/exec.js
@@ -0,0 +1,11 @@
+"use strict";
+function mutiplica(x, y) {
+    return x * y;
+}
+function ola(nome) {
+    return "olá" + nome;
+}
+const resultadoMutiplicacao = mutiplica(5, 7);
+const resultadoola = ola("fernanda");
+console.log(resultadoMutiplicacao);
+console.log(resultadoola);
---
+++ src/exec.ts
@@ -0,0 +1,13 @@
+function mutiplica (x:number, y:number ){
+    return x * y
+}
+
+function ola (nome: string): string{
+    return "olá"+ nome
+}
+
+const resultadoMutiplicacao = mutiplica(5,7);
+const resultadoola = ola("fernanda");
+
+console.log(resultadoMutiplicacao);
+console.log(resultadoola);
